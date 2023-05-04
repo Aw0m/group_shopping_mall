ffb4d6b74c9f3287abf7d99b609187b6d8b83cb6
Awom <liawom@foxmail.com>
2023-05-04 16:50:07 +0800

    fix: 修复User错误的AddressId，int -> int64

--- model/bdm/user.go
+++ model/bdm/user.go
@@ -11,13 +11,13 @@ import (
 
 // User 用户信息表
 type User struct {
-	UserId    int64  `gorm:"column:user_id;type:bigint(20);AUTO_INCREMENT;comment:用户自增id;primary_key" json:"user_id"`
+	UserId    int64  `gorm:"column:user_id;type:bigint(20);AUTO_INCREMENT;comment:用户自增id;primary_key" json:"user_id,string"`
 	OpenId    string `gorm:"column:open_id;type:varchar(255);NOT NULL" json:"open_id"`
 	Gender    string `gorm:"column:gender;type:enum('m','f','u');comment:用户性别，枚举值m/f/u 男/女/未知;NOT NULL" json:"gender"`
 	Username  string `gorm:"column:username;type:varchar(16);comment:用户名，长度需不大于16;NOT NULL" json:"username"`
 	AvatarUrl string `gorm:"column:avatar_url;type:varchar(255);comment:用户头像地址" json:"avatar_url"`
 	PhoneNum  string `gorm:"column:phone_num;type:varchar(16);comment:电话号码" json:"phone_num"`
-	AddressId int    `gorm:"column:address_id;type:int(11);comment:用户自提点id" json:"address_id"`
+	AddressId int64  `gorm:"column:address_id;type:bigint(20);comment:用户自提点id" json:"address_id,string"`
 }
 
 func (u *User) CreateToken(key string) string {
--- model/rdm/user.go
+++ model/rdm/user.go
@@ -2,13 +2,13 @@ package rdm
 
 // User 用户信息表
 type User struct {
-	UserId    int64  `json:"user_id"`    // 用户自增id
-	OpenId    string `json:"open_id"`    // 用户唯一标识
-	Gender    string `json:"gender"`     // 用户性别，枚举值m/f/u 男/女/未知
-	Username  string `json:"username"`   // 用户名，长度需不大于16
-	AvatarUrl string `json:"avatar_url"` // 用户头像地址
-	PhoneNum  string `json:"phone_num"`  // 电话号码
-	AddressId int    `json:"address_id"` // 用户自提点id
+	UserId    int64  `gorm:"column:user_id;type:bigint(20);AUTO_INCREMENT;comment:用户自增id;primary_key" json:"user_id"`
+	OpenId    string `gorm:"column:open_id;type:varchar(255);NOT NULL" json:"open_id"`
+	Gender    string `gorm:"column:gender;type:enum('m','f','u');comment:用户性别，枚举值m/f/u 男/女/未知;NOT NULL" json:"gender"`
+	Username  string `gorm:"column:username;type:varchar(16);comment:用户名，长度需不大于16;NOT NULL" json:"username"`
+	AvatarUrl string `gorm:"column:avatar_url;type:varchar(255);comment:用户头像地址" json:"avatar_url"`
+	PhoneNum  string `gorm:"column:phone_num;type:varchar(16);comment:电话号码" json:"phone_num"`
+	AddressId int64  `gorm:"column:address_id;type:bigint(20);comment:用户自提点id" json:"address_id,string"`
 }
 
 func (m *User) TableName() string {
